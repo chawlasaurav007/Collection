657c6ff66c0797b1a311c0b554d0b50fa65af2a8
Saurav Chawla <sauravchawla23@gmail.com>
2026-03-16 13:35:51 +0530

    custom arraylist created and use

---
+++ src/ArrayList/Custom_ArrayList.java
@@ -0,0 +1,82 @@
+package ArrayList;
+
+public class Custom_ArrayList {
+
+    Custom_ArrayList(int size){
+        arr = new Object[size];
+    }
+
+    Object[] arr = null;
+    int index = 0;
+
+    public void add(Object obj){
+
+        if(index >= arr.length){
+            int new_size = arr.length + (arr.length / 2);
+            Object[] arr2 = new Object[new_size];
+
+            for(int i = 0; i < arr.length; i++){
+                arr2[i] = arr[i];
+            }
+
+            arr = arr2;
+        }
+
+        arr[index] = obj;
+        index++;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+        s.append('[');
+
+        for(int i = 0; i < index; i++){
+            s.append(arr[i]);
+
+            if(i < index - 1){
+                s.append(", ");
+            }
+        }
+
+        s.append(']');
+        return s.toString();
+    }
+
+    public Object get(int index) throws Exception {
+
+        if(index >= this.index || index < 0){
+            throw new Exception("Index not found");
+        }
+
+        return arr[index];
+    }
+
+    public int size(){
+        return index;//We return index in the size() method because index keeps track of how many elements are currently
+                     // present in the array. Every time we add an element, index increases, and when we remove an
+                     // element, index decreases. So the value stored in index always represents the current size of
+                     // the list.
+    }
+
+    public void remove(int indexToBeDeleted) throws Exception {
+
+        if(indexToBeDeleted >= index || indexToBeDeleted < 0){
+            throw new Exception("Index not found");
+        }
+
+        Object[] o = new Object[arr.length - 1];
+
+        for(int i = 0; i < o.length; i++){
+            if(i < indexToBeDeleted){
+                o[i] = arr[i];
+            }else{
+                o[i] = arr[i + 1];
+            }
+        }
+
+        arr = o;
+        index--; // index tracks number of elements, and after removing one element we reduce it by 1
+    }
+}
---
+++ src/ArrayList/Custom_ArrayList_Use.java
@@ -0,0 +1,38 @@
+package ArrayList;
+
+public class Custom_ArrayList_Use {
+    public static void main(String[] args) throws Exception {
+        Custom_ArrayList c = new Custom_ArrayList(5);
+
+        c.add(1);
+        c.add(2);
+        c.add(3);
+        c.add(4);
+        c.add(5);
+        c.add(6);
+        c.add(7);
+        c.add(7);
+        c.add(3);
+
+        System.out.println(c);
+
+        System.out.println(c.get(5));
+
+        System.out.println("size is " + c.size());
+
+        c.remove(2);
+
+        System.out.println(c);
+
+        System.out.println("size is " + c.size());
+
+        System.out.println(c.get(5));
+
+        c.add(10);
+        c.add(12);
+
+        System.out.println(c);
+
+        System.out.println("size is " + c.size());
+    }
+}
