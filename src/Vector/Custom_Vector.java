ec3be83d43aefd92e824f6950f08ddd6e898b8e2
chawlasaurav007 <chawlasaurav007@gmail.com>
2026-03-16 16:33:57 +0530

    custom vector

---
+++ src/Vector/Custom_Vector.java
@@ -0,0 +1,78 @@
+package Vector;
+
+public class Custom_Vector {
+    Custom_Vector(int size){
+        arr = new Object[size];
+    }
+
+    Object[] arr = null;
+    int index = 0;
+
+    public synchronized void  add(Object obj){
+
+        if(index >= arr.length){
+            int new_size = arr.length + arr.length ;
+            Object[] arr2 = new Object[new_size];
+
+            for(int i = 0; i < arr.length; i++){
+                arr2[i] = arr[i];
+            }
+
+            arr = arr2;
+        }
+
+        arr[index] = obj;
+        index++;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+        s.append('[');
+
+        for(int i = 0; i < index; i++){
+            s.append(arr[i]);
+
+            if(i < index - 1){
+                s.append(", ");
+            }
+        }
+
+        s.append(']');
+        return s.toString();
+    }
+
+    public synchronized Object get(int index) throws Exception {
+
+        if(index >= this.index || index < 0){
+            throw new Exception("Index not found");
+        }
+
+        return arr[index];
+    }
+
+    public synchronized int size(){
+        return index;
+    }
+
+    public synchronized void remove(int indexToBeDeleted) throws Exception {
+
+        if(indexToBeDeleted >= index || indexToBeDeleted < 0){
+            throw new Exception("Index not found");
+        }
+
+        Object[] o = new Object[arr.length - 1];
+
+        for(int i = 0; i < o.length; i++){
+            if(i < indexToBeDeleted){
+                o[i] = arr[i];
+            }else{
+                o[i] = arr[i + 1];
+            }
+        }
+
+        arr = o;
+        index--;
+    }
+}
---
+++ src/Vector/Custom_Vector_Use.java
@@ -0,0 +1,40 @@
+package Vector;
+
+public class Custom_Vector_Use {
+    public static void main(String[] args) throws Exception {
+        Custom_Vector c = new Custom_Vector(5);
+
+        c.add(1);
+        c.add(2);
+        c.add(3);
+        c.add(4);
+        c.add(5);
+        c.add(6);
+        c.add(7);
+        c.add(7);
+        c.add(3);
+        c.add(14);
+        c.add(14);
+
+        System.out.println(c);
+
+        System.out.println(c.get(5));
+
+        System.out.println("size is " + c.size());
+
+        c.remove(2);
+
+        System.out.println(c);
+
+        System.out.println("size is " + c.size());
+
+        System.out.println(c.get(5));
+
+        c.add(10);
+        c.add(12);
+
+        System.out.println(c);
+
+        System.out.println("size is " + c.size());
+    }
+}
