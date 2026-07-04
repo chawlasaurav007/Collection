57f3d91e36325297b36f211c0fd4f61237486035
chawlasaurav007 <chawlasaurav007@gmail.com>
2026-07-04 17:28:58 +0530

    Balanced parenthesis question of stack

--- src/Stack/Balanced_Parenthesis.java
+++ src/Stack/Balanced_Parenthesis.java
@@ -4,20 +4,30 @@ import java.util.Stack;
 
 public class Balanced_Parenthesis {
     public static void main(String[] args) {
-        String s = "[]{([])}";
+        String s = "()";
         Stack<Character> s1 = new Stack<>();
         char[] charArray = s.toCharArray();
         for(int i=0;i<charArray.length;i++){
             if(charArray[i]=='{' || charArray[i]=='[' || charArray[i]=='('){
                 s1.push(charArray[i]);
-            }else if(charArray[i]=='}' || charArray[i]==']' || charArray[i]==')'){
-                s1.pop();
+            }else{
+               if(s1.isEmpty()){
+                   System.out.println("Not balanced");
+                   return;
+               }
+                char top = s1.pop();
+                if(charArray[i]=='}' && top!='{' ||
+                   charArray[i]==']'&& top!='[' ||
+                   charArray[i]==')' && top!='('){
+                    System.out.println("Not balanced");
+                    return ;
+                }
             }
         }
         if(s1.isEmpty()){
-            System.out.println("Its a balanced Parenthesis");
+            System.out.println("Balanced");
         }else {
-            System.out.println("Its not a balanced Parenthesis");
+            System.out.println("Not Balanced");
         }
     }
 
